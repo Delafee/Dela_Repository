093936b960e59f381a2052f33048437bbcf6d470
Delafee <31274913+Delafee@users.noreply.github.com>
2017-08-23 20:22:43 +0800

    Udate2

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -4,28 +4,8 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		zi z = new zi();
-		z.show();
-		z.show2();
-		//System.out.println(z.num+"..."+z.num);
+		System.out.println("Hello World!");
 	}
 }
 
-class Fu
-{
-	int num=4;
-}
 
-class zi extends Fu
-{
-	int num=5;
-	void show()
-	{
-		System.out.println(super.num);
-	}
-	
-	void show2()
-	{
-		System.out.println(this.num);
-	}
-}
